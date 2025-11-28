04ca87abf9cf2b9adb8afd7f3facc6925e5ef85c
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-28 20:00:48 -0300

    chore: configuration of svgr in defineConfig method • variable colors • configuration to typescript • overrides default configurations of svgo plugin • added reference and export set to const

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,41 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        ref: true,
+        exportType: "named",
+        jsxRuntime: "automatic",
+        typescript: true,
+        dimensions: false,
+
+        // Torna os valores das cores variáveis
+        replaceAttrValues: {
+          "#4682DA": "{props.primaryColor}",
+          "#EFEFEF": "{props.whiteColor}",
+          "#DDD83B": "{props.starsColor}",
+        },
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: "preset-default",
+              params: {
+                overrides: {
+                  cleanupIds: false,
+                  inlineStyles: {
+                    onlyMatchedOnce: false,
+                  },
+                },
+              },
+            },
+            "prefixIds", // Adiciona prefixo nos IDs
+            "convertStyleToAttrs", // Transforma CSS em atributos
+          ],
+        },
+      },
+    }),
+  ],
 });
