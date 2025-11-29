98c5fe1d237b50181c14c17160a3962de1297fc8
felipeFerreiraffl <felipado2x0@gmail.com>
2025-11-29 14:31:30 -0300

    chore: changed vite svgr configs • returned vite-plugin-svgr package • excluded typescript option

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
@@ -9,18 +10,9 @@ export default defineConfig({
     svgr({
       svgrOptions: {
         ref: true,
-        exportType: "named",
-        jsxRuntime: "automatic",
-        typescript: true,
+        exportType: "default",
         dimensions: false,
 
-        // Torna os valores das cores variáveis
-        replaceAttrValues: {
-          "#4682DA": "{props.primaryColor}",
-          "#EFEFEF": "{props.whiteColor}",
-          "#DDD83B": "{props.yellowColor}",
-        },
-        svgo: true,
         svgoConfig: {
           plugins: [
             {
@@ -28,17 +20,27 @@ export default defineConfig({
               params: {
                 overrides: {
                   cleanupIds: false,
-                  inlineStyles: {
-                    onlyMatchedOnce: false,
-                  },
                 },
               },
             },
-            "prefixIds", // Adiciona prefixo nos IDs
-            "convertStyleToAttrs", // Transforma CSS em atributos
           ],
+        },
+
+        // Torna os valores das cores variáveis
+        replaceAttrValues: {
+          "#4682DA": "currentColor",
+          "#EFEFEF": "currentColor",
+          "#DDD83B": "currentColor",
         },
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
 });
