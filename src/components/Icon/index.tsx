d15113547fd438328c48a1736d75381efb96abfc
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-01 14:41:19 -0300

    feat(Icon): created a new component to wrap phospor icons

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,26 @@
+import type { ComponentProps } from "react";
+import { type Icon as PhosphorIcon } from "@phosphor-icons/react";
+
+interface IconProps extends Omit<ComponentProps<PhosphorIcon>, "ref"> {
+  icon: PhosphorIcon;
+  size?: number;
+  weight?: "thin" | "light" | "regular" | "bold" | "fill" | "duotone";
+  className: string;
+}
+
+export default function Icon({
+  icon: IconComponent,
+  size = 24,
+  weight = "regular",
+  className = "",
+  ...props
+}: IconProps) {
+  return (
+    <IconComponent
+      className={className}
+      size={size}
+      weight={weight}
+      {...props}
+    />
+  );
+}
