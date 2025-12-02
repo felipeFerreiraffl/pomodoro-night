76cacef01145dad1f4d3c7d14dfc1c76c2ba4158
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-02 20:17:07 -0300

    feat: added logic ffor color changing in dropdown color menu • added context in color dropdown component • styled for active card

--- src/components/Header/ColorDropdown/index.tsx
+++ src/components/Header/ColorDropdown/index.tsx
@@ -1,25 +1,64 @@
+import { useTheme } from "@services/contexts/themeContext";
 import styles from "./styles.module.css";
 
 export default function ColorDropdown() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <ul className={styles.container} role="menu">
-      <li className={`${styles.item} ${styles.default}`} role="none">
-        <button role="menuitem" className={`${styles.card}`}>
+      <li
+        className={`${styles.item} ${styles.default} ${
+          theme === "default" ? styles.active : ""
+        }`}
+        role="none"
+      >
+        <button
+          role="menuitem"
+          className={styles.card}
+          onClick={() => toggleTheme("default")}
+        >
           Padrão
         </button>
       </li>
-      <li className={`${styles.item} ${styles.cyberpunk}`} role="none">
-        <button role="menuitem" className={styles.card}>
+      <li
+        className={`${styles.item} ${styles.cyberpunk} ${
+          theme === "cyberpunk" ? styles.active : ""
+        }`}
+        role="none"
+      >
+        <button
+          role="menuitem"
+          className={styles.card}
+          onClick={() => toggleTheme("cyberpunk")}
+        >
           Cyberpunk
         </button>
       </li>
-      <li className={`${styles.item} ${styles.florest}`} role="none">
-        <button role="menuitem" className={styles.card}>
+      <li
+        className={`${styles.item} ${styles.forest} ${
+          theme === "forest" ? styles.active : ""
+        }`}
+        role="none"
+      >
+        <button
+          role="menuitem"
+          className={styles.card}
+          onClick={() => toggleTheme("forest")}
+        >
           Floresta
         </button>
       </li>
-      <li className={`${styles.item} ${styles.desert}`} role="none">
-        <button role="menuitem" className={styles.card}>
+      <li
+        className={`${styles.item} ${styles.desert} ${
+          theme === "desert" ? styles.active : ""
+        }`}
+        role="none"
+      >
+        <button
+          role="menuitem"
+          className={styles.card}
+          onClick={() => toggleTheme("desert")}
+        >
           Deserto
         </button>
       </li>
