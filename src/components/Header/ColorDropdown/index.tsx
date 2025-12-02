0cbbf1f88ef3666d2c581b56baa3415d7b454e0c
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-02 20:27:06 -0300

    feat: added prop for color dropdown component • added setState to get state changes for closing menu

--- src/components/Header/ColorDropdown/index.tsx
+++ src/components/Header/ColorDropdown/index.tsx
@@ -1,16 +1,25 @@
 import { useTheme } from "@services/contexts/themeContext";
 import styles from "./styles.module.css";
+import { setStateToFalse } from "@/utils/setState";
 
-export default function ColorDropdown() {
+interface ColorDropdownProps {
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ColorDropdown({ setState }: ColorDropdownProps) {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <ul className={styles.container} role="menu">
+    <ul
+      role="menu"
+      className={styles.container}
+      onBlur={setStateToFalse(setState)}
+    >
       <li
+        role="none"
         className={`${styles.item} ${styles.default} ${
           theme === "default" ? styles.active : ""
         }`}
-        role="none"
       >
         <button
           role="menuitem"
@@ -21,10 +30,10 @@ export default function ColorDropdown() {
         </button>
       </li>
       <li
+        role="none"
         className={`${styles.item} ${styles.cyberpunk} ${
           theme === "cyberpunk" ? styles.active : ""
         }`}
-        role="none"
       >
         <button
           role="menuitem"
@@ -35,10 +44,10 @@ export default function ColorDropdown() {
         </button>
       </li>
       <li
+        role="none"
         className={`${styles.item} ${styles.forest} ${
           theme === "forest" ? styles.active : ""
         }`}
-        role="none"
       >
         <button
           role="menuitem"
@@ -49,10 +58,10 @@ export default function ColorDropdown() {
         </button>
       </li>
       <li
+        role="none"
         className={`${styles.item} ${styles.desert} ${
           theme === "desert" ? styles.active : ""
         }`}
-        role="none"
       >
         <button
           role="menuitem"
