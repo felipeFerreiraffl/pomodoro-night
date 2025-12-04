a15e04812e913d5368d9bc311888c4c4fbe2ff83
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-04 14:09:44 -0300

    feat: added types and enums for timer context

---
+++ src/types/timer.types.ts
@@ -0,0 +1,19 @@
+// Enums para status e fases do pomodoro
+export type TimerStatus = "IDLE" | "RUNNING" | "PAUSED";
+export type TimerPhase = "POMODORO" | "BREAK" | "LONG_BREAK";
+
+// Tipagem para timer global
+export interface TimerContextType {
+  timeLeft: number;
+  status: TimerStatus;
+  phase: TimerPhase;
+  pomodoroCount: number;
+  progress: number;
+  formattedTime: string;
+
+  // Funções para os botões
+  startTimer: () => void;
+  pauseTimer: () => void;
+  skipTimer: () => void;
+  resetTimer: () => void;
+}
