4e785ffb56409535a2081e5ceb4c36ad77d13fff
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-04 10:24:53 -0300

    feat(timer): added a component for the progress ring at timer (no functionality yet)

---
+++ src/components/Timer/ProgressRing/index.tsx
@@ -0,0 +1,46 @@
+import styles from "./styles.module.css";
+
+interface ProgressRingProps {
+  progress: number;
+  size?: number;
+  strokeWidth?: number;
+  color?: string;
+}
+
+export default function ProgressRing({
+  progress,
+  size = 320,
+  strokeWidth = 2,
+  color = "rgb(var(--color-primary))",
+}: ProgressRingProps) {
+  const radius = size / strokeWidth / 2;
+  const circumference = 2 * Math.PI * radius;
+
+  // Progresso baseado no offset (0 = 100%)
+  const offset = circumference - (progress / 100) * circumference;
+
+  return (
+    <svg className={styles.progressRing} width={size} height={size}>
+      <circle
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        fill="none"
+        stroke="rgb(var(--color-black))"
+        strokeWidth={strokeWidth}
+      />
+      <circle
+        className={styles.progressRingCircle}
+        cx={size / 2}
+        cy={size / 2}
+        r={radius}
+        fill="none"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeDasharray={circumference}
+        strokeDashoffset={offset}
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
