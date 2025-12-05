da61039931d10ae017a7a63f93f60b4e4b8dc7e2
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-05 20:35:43 -0300

    feat: added new layout component for routes logic

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,13 @@
+import Layout from "@/components/Layout";
 import Home from "@/pages/Home";
 import { Route, Routes } from "react-router";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
