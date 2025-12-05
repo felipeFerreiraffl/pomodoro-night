e507dbe804f87f98aeeb54653620d732ba0a92b4
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-05 11:46:50 -0300

    feat: created new custom confirm modal

---
+++ src/components/ConfirmModal/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles.module.css";
+
+interface ConfirmModalProps {
+  isOpen: boolean;
+  message: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+export default function ConfirmModal({
+  isOpen,
+  message,
+  onConfirm,
+  onCancel,
+}: ConfirmModalProps) {
+  //   if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <p className={styles.message}>{message}</p>
+        <div className={styles.buttonsContainer}>
+          <button
+            className={`${styles.button} ${styles.confirm}`}
+            onClick={onConfirm}
+            aria-label="confirmar"
+          >
+            Confirmar
+          </button>
+          <button
+            className={`${styles.button} ${styles.cancel}`}
+            onClick={onCancel}
+            aria-label="cancelar"
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
