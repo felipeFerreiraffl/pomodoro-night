9b41e093fd4afe90ce18ac172afb3dd9ae7cea01
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-05 19:33:10 -0300

    style: styled button component: • added hover animations • added logic to change button color based on type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,11 +6,19 @@ interface ButtonProps {
   type: "default" | "delete" | "save";
   label: string;
   icon?: Icon;
+  //   onClick: () => void;
 }
 
 export default function Button({ type, label, icon }: ButtonProps) {
+  const styleByType =
+    type === "delete"
+      ? styles.delete
+      : type === "save"
+      ? styles.save
+      : styles.default;
+
   return (
-    <button className={styles.button}>
+    <button className={`${styles.button} ${styleByType}`}>
       {icon && <CustomIcon icon={icon} size={20} weight="regular" />}
       <span className={styles.label}>{label}</span>
     </button>
