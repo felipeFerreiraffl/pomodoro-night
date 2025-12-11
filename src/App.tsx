93f4747251de1a8df17061e0e19b72dd027a21db
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-11 15:56:00 -0300

    refactor: envolved app in task context, before timer and after theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "@services/contexts/themeContext";
 import AppRoutes from "./routes/routes";
 import { TimerProvider } from "./services/contexts/timerContext";
+import { TaskProvider } from "./services/contexts/taskContext";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <TimerProvider>
-        <AppRoutes />
-      </TimerProvider>
+      <TaskProvider>
+        <TimerProvider>
+          <AppRoutes />
+        </TimerProvider>
+      </TaskProvider>
     </ThemeProvider>
   );
 }
