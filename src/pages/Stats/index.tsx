29340e6f38964072151595f9c03c5d2f7124b031
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-29 21:55:02 -0300

    feat: new section for main counting stats • styled section

--- src/pages/Stats/index.tsx
+++ src/pages/Stats/index.tsx
@@ -5,6 +5,35 @@ export default function Stats() {
     <>
       <main className={styles.main}>
         <h1 className={styles.mainTitle}>Suas estatísticas</h1>
+
+        <div className={styles.statsContainer}>
+          <section className={styles.mainStats}>
+            <div className={styles.countingStat}>
+              <h2 className={styles.countingTitle}>Total de pomodoros</h2>
+
+              <div className={styles.numberContainer}>
+                <p className={styles.number}>20</p>
+                <span className={styles.increment}>+2</span>
+              </div>
+            </div>
+            <div className={styles.countingStat}>
+              <h2 className={styles.countingTitle}>Tarefas concluídas</h2>
+
+              <div className={styles.numberContainer}>
+                <p className={styles.number}>5</p>
+                <span className={styles.increment}>+1</span>
+              </div>
+            </div>
+            <div className={styles.countingStat}>
+              <h2 className={styles.countingTitle}>Tempo focado</h2>
+
+              <div className={styles.numberContainer}>
+                <p className={styles.number}>1h 20 min</p>
+                <span className={styles.increment}>+4 min</span>
+              </div>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
