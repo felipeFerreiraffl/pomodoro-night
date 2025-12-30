e840e60eb016ff000efe81bff86460381ef0cc21
felipeFerreiraffl <felipado2x0@gmail.com>
2025-12-29 21:25:38 -0300

    feat: added new page into project • created new folder for stats page • added new page into routes file component

---
+++ src/pages/Stats/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export default function Stats() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1 className={styles.mainTitle}>Suas estatísticas</h1>
+      </main>
+    </>
+  );
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import Home from "@/pages/Home";
+import Stats from "@/pages/Stats";
 import { Route, Routes } from "react-router";
 
 export default function AppRoutes() {
@@ -7,6 +8,7 @@ export default function AppRoutes() {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
       </Route>
     </Routes>
   );
