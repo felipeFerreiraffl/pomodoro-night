24bdd2a6736a8a8542f8a247427ddb8bc1b6e3b5
felipeFerreiraffl <felipado2x0@gmail.com>
2026-01-09 19:25:30 -0300

    feat: new helpers for date and time methods • created method to get period of the day by hour

---
+++ src/utils/helpers.tsx
@@ -0,0 +1,6 @@
+export const getPeriodOfDay = (): "morning" | "afternoon" | "night" => {
+  const hour = new Date().getHours();
+  if (hour >= 6 && hour < 12) return "morning";
+  if (hour >= 12 && hour < 18) return "afternoon";
+  return "night";
+};
