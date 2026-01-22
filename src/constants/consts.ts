579d2871b3f3f6c2e7d9b50444ff7cace4bf01e1
felipeFerreiraffl <felipado2x0@gmail.com>
2026-01-22 10:45:10 -0300

    feat: added new constants for localStorage keys

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,2 +1,7 @@
 export const LAST_DAY = 86400000;
 export const LAST_WEEK = 86400000 * 7;
+
+export const THEME_STORAGE_KEY = "theme";
+export const TIMER_STORAGE_KEY = "pomodoroTimer";
+export const TASK_STORAGE_KEY = "pomodoroTasks";
+export const SESSIONS_STORAGE_KEY = "pomodoroSessions";
